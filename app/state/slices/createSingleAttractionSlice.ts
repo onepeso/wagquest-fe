c91e888f8cefe0b531ff6094994e6a617faf645a
onepeso <giovanicode@outlook.com>
2024-04-05 01:27:39 -0700

    add state for getting single attraction

---
+++ app/state/slices/createSingleAttractionSlice.ts
@@ -0,0 +1,11 @@
+export const createSingleAttractionSlice = (set: any) => ({
+    attraction: null,
+    fetchSingleAttraction: async (id) => {
+      //const headers = {'Authorization': 'Bearer ' + localStorage.getItem('accessToken')};
+      const res = await fetch(
+        `http://localhost:8080/attraction/${id}`, //{headers: headers}
+      );
+      const data = await res.json();
+      set({ attraction: data.attraction || null });
+    },
+  });
