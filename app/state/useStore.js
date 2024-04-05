3c51a71c3c4fe549d6b00d83a5529b6664d1b997
onepeso <giovanicode@outlook.com>
2024-04-05 01:28:14 -0700

    added redux dev tool middleware for zustand
    will help with visualizing the state and handling it better

--- app/state/useStore.js
+++ app/state/useStore.js
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 import { createAttractionSlice } from "./slices/createAttractionSlice";
+import { createSingleAttractionSlice } from "./slices/createSingleAttractionSlice";
+import { devtools } from "zustand/middleware";
 
-export const useStore = create((...a) => ({
-  ...createAttractionSlice(...a),
-}));
+export const useStore = create(
+  devtools((set) => ({
+    ...createAttractionSlice(set),
+    ...createSingleAttractionSlice(set),
+  }))
+);
