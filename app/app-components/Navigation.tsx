db611a27aea95a4a7572920e0c869b974d80590c
onepeso <giovanicode@outlook.com>
2024-04-08 21:04:57 -0700

    add user icon and clerk auth

--- app/app-components/Navigation.tsx
+++ app/app-components/Navigation.tsx
@@ -1,8 +1,9 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import React, { useState } from "react";
+import Link from "next/link";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -12,36 +13,51 @@ const Navigation = () => {
   };
 
   return (
-    <nav className="bg-[#0077B6]">
+    <nav className="bg-black">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
               <Link href="/">
-                <span className="text-white font-bold text-xl">Paw Adventures</span>
+                <span className="text-white font-bold text-xl">
+                  Paw Adventures
+                </span>
               </Link>
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link
                   href="/"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Discover
                 </Link>
                 <Link
                   href="/about"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   About
                 </Link>
                 <Link
                   href="/contact"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Contact
                 </Link>
               </div>
+            </div>
+          </div>
+          <div className="flex items-center">
+            {/* Clerk components */}
+            <div className="flex items-center">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+              <SignedOut>
+                <div className="text-white">
+                  <SignInButton />
+                </div>
+              </SignedOut>
             </div>
           </div>
           <div className="-mr-2 flex md:hidden">
@@ -62,8 +78,7 @@ const Navigation = () => {
           </div>
         </div>
       </div>
-
-      <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
+      <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
           <Link
             href="/"
