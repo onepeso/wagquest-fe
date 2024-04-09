bbe033e883ee52bd7c133193eaa9bffa08454650
onepeso <giovanicode@outlook.com>
2024-04-08 21:06:18 -0700

    Add Clerk Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./app-components/Navigation";
+import { ClerkProvider, clerk } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Paw Adventures",
-  description: "Explore a world of dog-friendly locations and experiences with PawAdventures. Find the perfect dog parks, trails, cafes, and more. Connect with fellow dog owners and embark on new adventures with your furry friend.",
-  };
+  description:
+    "Explore a world of dog-friendly locations and experiences with PawAdventures. Find the perfect dog parks, trails, cafes, and more. Connect with fellow dog owners and embark on new adventures with your furry friend.",
+};
 
 export default function RootLayout({
   children,
@@ -15,12 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      
-      <body className={inter.className}>
-        <Navigation/>{children}
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Navigation />
+          {children}
         </body>
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
-
