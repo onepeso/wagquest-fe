f72b2986c785583e5be452bfcc1f5838699b22c2
onepeso <giovanicode@outlook.com>
2024-04-11 02:23:29 -0700

    Fixed Import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navigation from "./app-components/Navigation";
+import Navigation from "./app-components/navigation-system/Navigation";
 import { ClerkProvider, clerk } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
