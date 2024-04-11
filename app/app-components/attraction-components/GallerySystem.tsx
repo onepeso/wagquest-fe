f35320e37e39acc2c53ffc92815ff5d1d80ad06a
onepeso <giovanicode@outlook.com>
2024-04-11 02:20:49 -0700

    Add Gallery System

---
+++ app/app-components/attraction-components/GallerySystem.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+const GallerySystem = ({ attraction }) => {
+  return (
+    <>
+      <div className="sm:col-span-2 md:col-span-3">
+        {attraction?.images[0] && (
+          <Image
+            src={attraction?.images[0]}
+            alt={attraction?.name}
+            width={800}
+            height={400}
+            className="rounded-md h-60 sm:h-fit object-cover"
+            loading="lazy"
+          />
+        )}
+      </div>
+      <div className="sm:col-span-2 grid grid-cols-2 gap-3">
+        {attraction?.images
+          .slice(1, 5)
+          .map((image, index) => (
+            <Image
+              key={index}
+              src={image}
+              alt={attraction?.name}
+              width={400}
+              height={300}
+              className="rounded-md h-56 sm:h-full object-cover"
+              loading="lazy"
+            />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default GallerySystem;
