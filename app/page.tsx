5d134fb2fe428bea79cc034e6e4f9d54a7a706fa
onepeso <giovanicode@outlook.com>
2024-04-11 02:23:47 -0700

    Refactor Component Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,16 @@
-'use client';
+"use client";
 
-import AttractionCard from './app-components/AttractionCard';
-import HomeHeader from './app-components/HomeHeader';
-import { useStore } from './state/useStore';
-import { useEffect } from 'react';
+import AttractionCard from "./app-components/cards/AttractionCard";
+import HomeHeader from "./app-components/homepage-components/HomeHeader";
 
 export default function Home() {
-
-  const {fetchAttractions, attractions} = useStore();
-
-  useEffect(() => {
-    fetchAttractions();
-  }, [fetchAttractions]);
-
-  console.log(attractions);
-
   return (
     <main className="flex flex-col items-center">
-      <section className='w-full'>
+      <section className="w-full">
         <HomeHeader />
       </section>
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 w-full max-w-7xl px-4">
-      {attractions.length > 0 ? (
-          attractions.map((attraction) => (
-            <AttractionCard key={attraction.id} attraction={attraction} />
-          ))
-        ) : (
-          <p>No attractions found.</p>
-        )}
+        <AttractionCard />
       </section>
     </main>
   );
