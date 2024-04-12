a32fa58da75fa4a2429d72bb1bfc23ea718d8715
onepeso <giovanicode@outlook.com>
2024-04-12 09:52:05 -0700

    New UI for the Navigation System
    - Correct Colors
    - Sign In Button Now Shows In Larger Screens
    - Avatar of Logged in User Shows in Larger Screens
    - Other small QOF improvements

--- app/app-components/navigation-system/Navigation.tsx
+++ app/app-components/navigation-system/Navigation.tsx
@@ -13,20 +13,20 @@ const Navigation = () => {
   };
 
   return (
-    <nav className="bg-black">
+    <nav className="bg-[#eb5e28]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
               <Link href="/">
-                <span className="text-white font-bold text-xl">Wag Quest</span>
+                <span className="text-white font-bold text-xl">WagQuest</span>
               </Link>
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <Link
                   href="/hotels"
-                  className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-white hover:bg-[#403d39] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Hotels
                 </Link>
@@ -51,7 +51,7 @@ const Navigation = () => {
               </div>
             </div>
           </div>
-          <div className="-mr-2 flex gap-3 items-center md:hidden">
+          <div className="-mr-2 flex gap-3 items-center ">
             {/* Clerk components */}
             <SignedIn>
               <UserButton />
@@ -61,20 +61,22 @@ const Navigation = () => {
                 <SignInButton />
               </div>
             </SignedOut>
-            <button
-              onClick={toggleMenu}
-              type="button"
-              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded={isOpen}
-            >
-              <span className="sr-only">Open main menu</span>
-              {isOpen ? (
-                <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-              ) : (
-                <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-              )}
-            </button>
+            <div className="md:hidden">
+              <button
+                onClick={toggleMenu}
+                type="button"
+                className="bg-[#403d39] inline-flex items-center justify-center p-2 rounded-md text-white hover:text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded={isOpen}
+              >
+                <span className="sr-only">Open main menu</span>
+                {isOpen ? (
+                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -82,25 +84,25 @@ const Navigation = () => {
         <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
           <Link
             href="/hotels"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            className="text-white hover:bg-[#403d39] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
             Hotels
           </Link>
           <Link
             href="/restaurants"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            className="text-white hover:bg-[#403d39] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
             Restaurants
           </Link>
           <Link
             href="/activities"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            className="text-white hover:bg-[#403d39] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
             Activities
           </Link>
           <Link
             href="/events"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            className="text-white hover:bg-[#403d39] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
           >
             Events
           </Link>
