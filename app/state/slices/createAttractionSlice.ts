4e21a421e4526947380a8e7e69d88f48815aa4d2
onepeso <giovanicode@outlook.com>
2024-04-24 19:06:38 -0700

    More Testing of PROD BaseURL

--- app/state/slices/createAttractionSlice.ts
+++ app/state/slices/createAttractionSlice.ts
@@ -1,16 +1,20 @@
 import baseUrl from "@/lib/baseUrl";
 
-export const createAttractionSlice = (set: any) => (
-  console.log(baseUrl, "baseUrl"),
-  {
-    attractions: [],
-    fetchAttractions: async () => {
-      //const headers = {'Authorization': 'Bearer ' + localStorage.getItem('accessToken')};
-      const res = await fetch(
-        `${baseUrl}/attractions` //{headers: headers}
-      );
-      const data = await res.json();
-      set({ attractions: data.attractions || [] });
-    },
-  }
-);
+export const createAttractionSlice = (set: any) => {
+  const slice = async () => {
+    console.log(baseUrl, "baseUrl");
+    if (!baseUrl) {
+      throw new Error("baseUrl is not defined");
+    }
+    return {
+      attractions: [],
+      fetchAttractions: async () => {
+        //const headers = {'Authorization': 'Bearer ' + localStorage.getItem('accessToken')};
+        const res = await fetch(`${baseUrl}/attractions`); //{headers: headers}
+        const data = await res.json();
+        set({ attractions: data.attractions || [] });
+      },
+    };
+  };
+  return slice;
+};
