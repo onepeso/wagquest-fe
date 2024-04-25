b65d4417e7ae1c1ef05fb47771f696fd9f2478b4
onepeso <giovanicode@outlook.com>
2024-04-24 19:29:53 -0700

    seeing if this works

--- app/state/slices/createAttractionSlice.ts
+++ app/state/slices/createAttractionSlice.ts
@@ -1,16 +1,15 @@
-import baseUrl from "@/lib/baseUrl";
-
-export const createAttractionSlice = (set: any) => (
-  console.log(baseUrl, "baseUrl"),
-  {
-    attractions: [],
-    fetchAttractions: async () => {
-      //const headers = {'Authorization': 'Bearer ' + localStorage.getItem('accessToken')};
-      const res = await fetch(
-        `${baseUrl}/attractions` //{headers: headers}
-      );
-      const data = await res.json();
-      set({ attractions: data.attractions || [] });
-    },
-  }
-);
+export const createAttractionSlice = (set: any) => ({
+  attractions: [],
+  fetchAttractions: async () => {
+    const baseUrl =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : process.env.NEXT_PUBLIC_PROD_API_URL;
+    //const headers = {'Authorization': 'Bearer ' + localStorage.getItem('accessToken')};
+    const res = await fetch(
+      `${baseUrl}/attractions` //{headers: headers}
+    );
+    const data = await res.json();
+    set({ attractions: data.attractions || [] });
+  },
+});
