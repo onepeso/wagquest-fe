622c50278b50b4c3365696599660f768ff977af1
onepeso <giovanicode@outlook.com>
2024-04-26 14:09:51 -0700

    new global footer system

---
+++ app/app-components/footer-system/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bottom-0 left-0 w-full bg-white py-5">
+      <p className="text-center text-gray-500">
+        &copy; {new Date().getFullYear()} WagQuest. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
