04f521264ebf7c5f16c0d67b08d14033a122bfe4
onepeso <giovanicode@outlook.com>
2024-04-25 20:47:21 -0700

    WIP

--- lib/operatingHoursUtils.ts
+++ lib/operatingHoursUtils.ts
@@ -1,18 +1,27 @@
 import moment from "moment";
+import { OperatingHours } from "@/types/Common";
 
-export const isWithinOpeningHours = (
-  openTime: moment.Moment,
-  closeTime: moment.Moment,
-  currentTime: moment.Moment
-): boolean => {
+function isWithinOpeningHours(
+  operatingHours: any,
+  currentTime: string,
+  date: string = moment().format("YYYY-MM-DD")
+): boolean {
+  if (!operatingHours) return false;
+
+  const openTime = moment(operatingHours.open_time, "h:mm A").tz(
+    moment.tz.guess()
+  ); // Convert open time to local time zone
+  const closeTime = moment(operatingHours.close_time, "h:mm A").tz(
+    moment.tz.guess()
+  ); // Convert close time to local time zone
+  const currentMoment = moment(currentTime, "h:mm A"); // Convert current time to a moment object
+
+  // Check if close time is before open time (spanning across midnight)
   if (closeTime.isBefore(openTime)) {
-    // Closing time is on the next day
-    return (
-      currentTime.isBetween(openTime, moment().endOf("day")) ||
-      currentTime.isBetween(moment().startOf("day"), closeTime.add(1, "day"))
-    );
-  } else {
-    // Closing time is on the same day
-    return currentTime.isBetween(openTime, closeTime);
+    closeTime.add(1, "day"); // Adjust close time to be the next day
   }
-};
+
+  return currentMoment.isBetween(openTime, closeTime, "hour", "[]");
+}
+
+export default isWithinOpeningHours;
