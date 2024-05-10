9c0997136f4e85f47906a38b14ac1af6dd2ac15e
onepeso <giovanicode@outlook.com>
2024-05-09 23:35:07 -0700

    update ui for location sect

--- app/app-components/attraction-components/LocationUI.tsx
+++ app/app-components/attraction-components/LocationUI.tsx
@@ -2,15 +2,15 @@ import { Card, CardContent } from "@/components/ui/card";
 
 const LocationUI = ({ attraction }: any) => {
   return (
-    <section className="border-none rounded mb-4">
+    <section className="border-none rounded mb-8">
       <section className="flex flex-col space-y-4 md:flex-row md:justify-between md:items-center md:space-y-0 md:space-x-8 p-0">
         <div className="flex flex-col md:flex-row md:items-center">
-          <address className="text-lg font-bold not-italic">
+          <address className="text-lg font-semibold not-italic">
             {attraction?.data?.location}
           </address>
         </div>
         <div className="hidden md:flex items-center space-x-2">
-          <h2 className="text-xl font-bold text-gray-800">Price:</h2>
+          <h2 className="text-xl font-semibold text-gray-800">Price Range:</h2>
           <p className="text-lg text-main font-semibold">
             {attraction?.data?.prices?.lowestPrice === 0 &&
             attraction?.data?.prices?.highestPrice === 0 ? (
