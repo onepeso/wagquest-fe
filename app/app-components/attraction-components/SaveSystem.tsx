a4b80fe4c9378a785a20d8cfd0496da40da3b5b3
onepeso <giovanicode@outlook.com>
2024-05-09 20:32:23 -0700

    add icon to save button

--- app/app-components/attraction-components/SaveSystem.tsx
+++ app/app-components/attraction-components/SaveSystem.tsx
@@ -6,13 +6,17 @@ import {
 } from "@/components/ui/tooltip";
 
 import { Button } from "@/components/ui/button";
+import { HeartIcon } from "lucide-react";
 
 const SaveSystem = () => {
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
-          <Button variant="outline"> Save </Button>
+          <Button variant="outline">
+            <HeartIcon className="w-4 h-4 mr-1" />
+            <span className="hidden lg:block">Save</span>
+          </Button>
         </TooltipTrigger>
         <TooltipContent>
           <p>Feature Coming Soon</p>
