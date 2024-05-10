c6353061cedda6fcd14050d38dc7c7775ef69a54
onepeso <giovanicode@outlook.com>
2024-05-10 16:15:25 -0700

    add image to home header + ui fixes

--- app/app-components/homepage-components/HomeHeader.tsx
+++ app/app-components/homepage-components/HomeHeader.tsx
@@ -1,26 +1,32 @@
 import { ArrowDownIcon } from "lucide-react";
 import React from "react";
+import Image from "next/image";
 
 const HomeHeader = () => {
   return (
-    <header className="relative bg-cover bg-center w-full py-20 md:py-20 flex items-center justify-center bg-[#F5F5F5]">
-      <div className="absolute inset-0 bg-[#F5F5F5]"></div>
-      <div className="relative z-10 max-w-3xl px-4 mx-auto text-center">
-        <h1 className="text-2xl md:text-7xl font-bold mb-2 text-[#403d39] capitalize">
-          <span className="text-main">Unleash</span> the Adventure
-        </h1>
+    <header className=" bg-[#F5F5F5] bg-cover bg-center w-full py-8 md:py-20 flex flex-col items-center justify-center">
+      <div className="max-w-2xl px-4 mx-auto text-center">
+        <div className="flex flex-col md:flex md:flex-row-reverse items-center">
+          <div className="pb-5">
+            <Image
+              src="/nature.svg"
+              alt="hero"
+              width={300}
+              height={600}
+              className="w-40 md:w-48"
+            />
+          </div>
+          <h1 className="text-2xl md:text-7xl font-bold mb-2 text-[#403d39] capitalize w-fit">
+            <span className="text-main">Unleash</span> the Adventure
+          </h1>
+        </div>
+
         <p className="mb-6 text-base text-black md:text-lg">
           Explore a world of dog-friendly locations and experiences with
           WagQuest. Find the perfect dog parks, trails, cafes, and more. Connect
           with fellow dog owners and embark on new adventures with your furry
           friend.
         </p>
-        <button className="font-bold py-2 px-6 rounded-md hover:bg-main transition-colors duration-300 text-white bg-main">
-          <div className="flex gap-1">
-            <ArrowDownIcon />
-            Start Exploring
-          </div>
-        </button>
       </div>
     </header>
   );
