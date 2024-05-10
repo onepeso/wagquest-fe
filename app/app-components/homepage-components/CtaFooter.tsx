07bdaee79f5628dd2bfda615202840eb4e320f33
onepeso <giovanicode@outlook.com>
2024-05-09 20:33:49 -0700

    add correct main color

--- app/app-components/homepage-components/CtaFooter.tsx
+++ app/app-components/homepage-components/CtaFooter.tsx
@@ -3,8 +3,8 @@ const CtaHeader = () => {
     <header className="relative bg-cover bg-center w-full py-20 md:py-20 flex items-center justify-center">
       <div className="relative z-10 text-center max-w-3xl mx-auto px-4">
         <h1 className="text-2xl md:text-4xl font-bold mb-2 text-[#403d39] capitalize">
-          <span className="text-[#eb5e28] capitalize">do less searching</span>{" "}
-          and do more with your furry friend
+          <span className="text-main capitalize">do less searching</span> and do
+          more with your furry friend
         </h1>
       </div>
     </header>
