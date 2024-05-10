1bdacfd65489f852fc6121acecc7d74685270550
onepeso <giovanicode@outlook.com>
2024-05-09 23:34:55 -0700

    update ui for details

--- app/app-components/attraction-components/DetailsUI.tsx
+++ app/app-components/attraction-components/DetailsUI.tsx
@@ -4,35 +4,20 @@ import { PawPrint } from "lucide-react";
 const DetailsUI = ({ attraction }: any) => {
   return (
     <>
-      <Alert className="bg-slate-50 border border-slate-200 px-4 py-3 rounded-md mb-4">
-        <AlertTitle className="font-bold mb-1">
-          Dive into the details
-        </AlertTitle>
-        <AlertDescription>
-          This is where you'll find an in-depth breakdown of what makes this
-          particular location special and unique.
-        </AlertDescription>
-      </Alert>
-
+      <h3 className="scroll-m-20 my-8 text-2xl font-semibold tracking-tight">
+        What this place offers
+      </h3>
       {attraction?.data?.details?.length > 0 ? (
-        <ul className="mt-4 space-y-4 px-3">
+        <div className="mt-2 mb-8 grid grid-cols-2">
           {attraction?.data?.details?.map((detail: any, index: number) => (
-            <li
-              key={index}
-              className={`flex flex-col md:flex-row p-4 rounded-lg ${
-                index % 2 === 0 ? "bg-blue-50" : "bg-green-50"
-              }`}
-            >
-              <strong className="font-bold text-sm mb-1 md:mb-0 md:mr-2 text-gray-80 flex gap-2 items-center">
+            <div key={index} className="flex flex-col md:flex-row rounded-lg">
+              <p className="text-base mb-1 md:mb-0 md:mr-2 text-gray-80 flex gap-2 items-center">
                 <PawPrint />
-                {detail.detailTitle}:
-              </strong>
-              <span className="text-gray-700 text-sm">
-                {detail.detailDescription}
-              </span>
-            </li>
+                {detail.detailTitle}
+              </p>
+            </div>
           ))}
-        </ul>
+        </div>
       ) : (
         <p className="mt-4 text-gray-600 italic">
           No details available for this attraction.
