dea56c46759a963df375a500e1557ad896c58038
onepeso <giovanicode@outlook.com>
2024-05-09 20:31:30 -0700

    improve the UI of the AttractionHeader
    This visual improvements will create a better user experience.
    
    Other components inside this component were effected.

--- app/app-components/attraction-components/AttractionHeader.tsx
+++ app/app-components/attraction-components/AttractionHeader.tsx
@@ -3,16 +3,16 @@ import ShareSystem from "./ShareSystem";
 
 const AttractionHeader = ({ attraction }: any) => {
   return (
-    <header className="flex flex-col md:flex-row md:items-center md:justify-between lg:mb-4">
+    <header className="flex md:items-center justify-between lg:mb-4">
       <div className="mb-4 md:mb-0">
-        <h1 className="flex items-center mb-2 text-xl font-bold text-gray-900 sm:text-2xl md:text-3xl">
+        <h1 className="flex items-center mb-2 text-gray-900 text-xl scroll-m-20 md:text-3xl font-extrabold tracking-tight lg:text-4xl">
           {attraction?.data?.title}
         </h1>
-        <p className="hidden text-sm text-gray-700 lg:block md:text-base">
+        <p className="hidden text-gray-700 lg:block leading-7 [&not(:first-child)]:mt-6">
           {attraction?.data?.overview}
         </p>
       </div>
-      <div className="hidden gap-3 lg:flex md:gap-4">
+      <div className="gap-3 flex ">
         <ShareSystem attraction={attraction} />
         <SaveSystem />
       </div>
