cfb5787fbb1860c142e99ab9d4b8de69f4feb69c
onepeso <giovanicode@outlook.com>
2024-05-10 00:49:11 -0700

    fix next config to handle domains with remotePatters

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.pexels.com", "www.pexels.com", "cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        //port: use in future if needed
+        //pathname: use in future if needed
+      },
+      {
+        protocol: "https",
+        hostname: "www.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
   },
 };
 
