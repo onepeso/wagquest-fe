953e9caa770d5f171bfc92e524c791efad4685bc
onepeso <giovanicode@outlook.com>
2024-05-09 20:34:38 -0700

    improve the Map to handle non browser issue
    fixed: window undefined issue

--- app/app-components/map/Map.js
+++ app/app-components/map/Map.js
@@ -1,25 +1,39 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { FaMapPin } from "react-icons/fa";
-import L from "leaflet";
-import ReactDOMServer from "react-dom/server";
-
-const customIcon = L.divIcon({
-  className: "custom-icon",
-  html: ReactDOMServer.renderToString(<FaMapPin size={40} color="red" />),
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-});
+import { useState, useEffect } from "react";
 
 const Map = ({ address, latitude, longitude }) => {
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
+
+  if (!isBrowser) {
+    return null; // Return null during server-side rendering
+  }
+
+  const { MapContainer, TileLayer, Marker, Popup } = require("react-leaflet");
+  const { FaMapPin } = require("react-icons/fa");
+  const L = require("leaflet");
+  const ReactDOMServer = require("react-dom/server");
+
+  const customIcon = L.divIcon({
+    className: "custom-icon",
+    html: ReactDOMServer.renderToString(<FaMapPin size={40} color="red" />),
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+  });
+
   if (!latitude || !longitude) {
     return <div>Location data not available</div>;
   }
+
   return (
-    <div className="z-10 relative" style={{ height: "400px", width: "full" }}>
+    <div className="relative" style={{ height: "400px", width: "full" }}>
       <MapContainer
         center={[latitude, longitude]}
         zoom={16}
+        scrollWheelZoom={false}
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
