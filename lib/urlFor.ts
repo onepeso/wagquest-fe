6ab00bd9d141758e76724a2799e5ebee954a9b3e
onepeso <giovanicode@outlook.com>
2024-05-15 20:54:40 -0700

    add auto-format to images from sanity

--- lib/urlFor.ts
+++ lib/urlFor.ts
@@ -4,8 +4,12 @@ import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder(client);
 
-function urlFor(source: Post) {
-  return builder.image(source);
+function urlFor(source: Post, width?: number, height?: number) {
+  let url = builder.image(source);
+  if (width && height) {
+    url = url.width(width).height(height).auto("format");
+  }
+  return url.url();
 }
 
 export default urlFor;
