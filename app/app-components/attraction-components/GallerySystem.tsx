39b8cd12f068bb08b91fc02b4e36f6dc849340d6
onepeso <giovanicode@outlook.com>
2024-05-16 14:46:42 -0700

    add new gallery system with correct sanity type

--- app/app-components/attraction-components/GallerySystem.tsx
+++ app/app-components/attraction-components/GallerySystem.tsx
@@ -1,18 +1,36 @@
 import urlFor from "@/lib/urlFor";
 import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const GallerySystem = ({ attraction }: any) => {
   return (
     <>
-      <section className="w-full">
-        <Image
-          src={urlFor(attraction?.mainImage, 600, 400)}
-          alt="Attraction Image"
-          width={600}
-          height={400}
-          className="rounded-lg w-full h-[500px] object-cover"
-          priority
-        />
+      <section className="w-full p-5">
+        <Carousel>
+          <CarouselContent>
+            {attraction?.gallery.map((image: any, index: number) => (
+              <CarouselItem key={index}>
+                <Image
+                  src={urlFor(image, 600, 400)}
+                  alt="Attraction Image"
+                  width={600}
+                  height={400}
+                  className="rounded-lg w-full h-[500px] object-cover"
+                  priority
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </section>
     </>
   );
